8fe171d6c3cfd9b09bee2a1a2f0e2b2fa73514fa
synth <synthalorian@gmail.com>
2026-04-09 20:10:54 -0400

    synth update: synchronization and release preparation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 import 'package:open_life/main.dart';
 
 void main() {
+  setUp(() {
+    Animate.restartOnHotReload = false;
+  });
+
   testWidgets('App launches smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const ProviderScope(child: OpenLifeApp()));
-    expect(find.text('Open Life'), findsWidgets);
+    await tester.pump(const Duration(seconds: 1));
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
